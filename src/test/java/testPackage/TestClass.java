00a5d3ca1706ff6c3500cb4143e56ad6596a88ad
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-08-23 14:44:00 +0300

    day 1: final commit

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -2,6 +2,8 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class TestClass {
@@ -9,14 +11,23 @@ public class TestClass {
     public void testMethod() {
         System.out.println("Test method executed");
 
-        WebDriver driver;
+        WebDriver driver;  //13 methods - cross-browser
+//        RemoteWebDriver driver; //45 methods - cross-browser
+//        ChromiumDriver driver; //x methods - Chromium-based browsers
+//        ChromeDriver driver; //x methods - Chrome browser
         driver = new ChromeDriver();
-        System.out.println(driver);
+
+        driver.get("https://www.google.com");
+        driver.navigate().to("https://www.google.com");
+
+//        System.out.println(driver);
         driver.quit();
+//
+//        WebDriver driver2 = new ChromeDriver();
+//        driver2.quit();
+//
+//        new ChromeDriver().quit();
 
-        WebDriver driver2 = new ChromeDriver();
-        driver2.quit();
-
-        new ChromeDriver().quit();
+//        WebDriver driver;
     }
 }
